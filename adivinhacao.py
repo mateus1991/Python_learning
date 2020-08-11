2540bc98203840986d1e1b83e2d587b48352c240
mateus1991 <mateus.oliveira@gec.inatel.br>
2020-08-11 14:08:40 -0300

    Finalizando jogo de adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -2,8 +2,23 @@ print('********************************')
 print('Bem vindo ao jogo de adivinhação')
 print('********************************')
 
-segredo = 42
-tentativas = 3
+import random
+
+segredo = random.randint(1,100)  #  gera entre 1 e 100
+pontuacao = 1000
+tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("1 - Fácil  2 - Médio  3 - Difícil")
+
+nivel = int(input("Defina seu nivel: "))
+
+if(nivel == 1):
+    tentativas = 20
+elif(nivel == 2):
+    tentativas = 10
+else:
+    tentativas = 5
 
 
 for rodada in range(1,tentativas + 1):
@@ -19,7 +34,7 @@ for rodada in range(1,tentativas + 1):
     acerto = segredo == chute
 
     if(acerto):
-        print('Você acertou!')
+        print('Você acertou e fez {}'.format(pontuacao))
         break
     else:
         maior = chute > segredo
@@ -27,6 +42,8 @@ for rodada in range(1,tentativas + 1):
             print('Você errou! Seu chute foi maior que o número secreto.')
         else:
             print('Você errou! Seu chute foi menor que o número secreto.')
+        pontos_perdidos = abs(segredo - chute)
+        pontuacao = pontuacao - pontos_perdidos
 
 
 print('Fim do jogo.')
